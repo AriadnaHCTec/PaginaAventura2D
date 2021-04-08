7d74ded073bf9516bede1bc7b15e00213fcff171
Javi <a01750400@itesm.mx>
2021-04-08 10:39:22 -0500

    xd

---
+++ routes/usuario.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const usuarioController = require('../controllers/usuario')
+//mini app
+const router = express.Router();
+
+//Mostrar el formulario
+router.get('/agregarUsuario',usuarioController.getAgregarUsuario);
+
+//Obtener los datos del formulario
+router.post('/agregarUsuario',usuarioController.postAgregarUsuario);
+
+//Mostrar al usuario el resultado de la transaccion
+router.get('/confirmacion',usuarioController.getConfirmacion);
+
+//Mostrar los registros de la base de datos
+router.get('/registros',usuarioController.getRegistros);
+
+module.exports =router;
