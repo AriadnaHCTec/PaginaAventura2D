a451e0be09922c5833cfb035f2fbdde4ac45201e
Miguel Pérez <a01750145@itesm.mx>
2021-04-19 21:47:25 -0500

    model jugador

---
+++ models/Jugador.js
@@ -0,0 +1,61 @@
+const Sequelize = require('sequelize');
+//Traer el objeto sequelize
+const sequelize = require('../util/database');
+
+const Usuario = sequelize.define('Usuario',{
+    nombreUsuario:{
+        type: Sequelize.STRING(50),
+        allowNull: false,
+        primaryKey: true   
+    },
+    correo:{
+        type: Sequelize.STRING(250),
+        allowNull: false
+    },
+    contraseña:{
+        type: Sequelize.STRING(50),
+        allowNull: false
+    },
+    fechaDeNacimiento:{
+        type: Sequelize.DATE,
+        allowNull: false
+    },
+    genero:{
+        type: Sequelize.STRING(1),
+        allowNull: false
+    },
+    profesion:{
+        type: Sequelize.STRING(50),
+        allowNull: false
+    },
+    estado:{
+        type: Sequelize.STRING,
+        allowNull: true
+    },    
+    fechaRegistro:{
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: new Date()
+    },
+    ultimaSesion:{
+        type: Sequelize.DATE,
+        allowNull: true
+    },
+    tiempoJuego:{
+        type: Sequelize.TIME,
+        allowNull: true
+    },
+    puntos:{
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        defaultValue: 0
+    },
+    estadoJuego:{
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    }
+
+});
+
+module.exports = Usuario;
